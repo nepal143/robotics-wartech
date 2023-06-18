766341aaa09e126e1aadbac514386ce0c20853a3
Nepal Singh <71310255+nepal143@users.noreply.github.com>
2023-06-18 11:17:29 +0530

    tried updating things

--- schedules/assets/Script/schedules.js
+++ schedules/assets/Script/schedules.js
@@ -1,15 +1,17 @@
 const boxes = document.getElementsByClassName('box'); 
-const lines = document.getElementsByClassName('line');
+
 
 const checkinwindow = ()=>{
     var windows_height = (window.scrollY + window.innerHeight)/5*4;
+
+    
     // console.log(windows_height);
     for(var i = 0 ;i <  boxes.length ;i++){
         var box_top = boxes[i].getBoundingClientRect().top ;
         if(box_top < windows_height){
             boxes[i].classList.add('show');
             let classid  ="line"+ (boxes[i].classList[0]);
-            console.log(classid);
+            // console.log(classid);
             document.getElementById(classid).classList.add('length');
             
         }
