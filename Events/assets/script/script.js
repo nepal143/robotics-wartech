94f3a4b42e9b932a058744bb9b6abcd9959701cb
Nepal Singh <nepalsss007@gmail.com>
2023-08-19 11:56:31 +0530

    added something

--- Events/assets/script/script.js
+++ Events/assets/script/script.js
@@ -7,4 +7,18 @@ const rotate = (degree)=>{
         document.getElementById(id).style.rotate = (degree1+"deg");
         document.getElementById((id+'p')).style.rotate = ("-"+degree1+"deg");
     }
+    flip(degree/72 -1);
+}
+let rotation = 0 ;
+const data  = ["ONE" , "TWO" , "THREE" ,"FOUR" ,"FIVE" ]
+const main_card = document.getElementsByClassName("main-pentagon");
+const problem = document.getElementById("problem")
+let flip = (index)=>{
+    rotation+= 1 ;
+    if(rotation%2 ==0)
+        main_card[0].style.transform  = "rotateY(360deg)";
+    else{
+        main_card[0].style.transform  = "rotateY(0deg)";
+    }
+    problem.innerText = data[index];
 }
